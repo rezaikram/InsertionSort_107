6c2accd6c7eda75f0e1de0a77fd56e50de9f2eeb
rezaikram <muhammadikramriza@gmail.com>
2023-03-23 22:12:42 +0700

    membuat element array

--- Activity Insertion Sort/Activity Insertion Sort.cpp
+++ Activity Insertion Sort/Activity Insertion Sort.cpp
@@ -4,3 +4,16 @@ using namespace std;
 int arr[20];	// Membuat Array dengan panjang data 20
 int n;			// Membuat variabel inputan n 
 
+void input()
+{	// Procedure input
+	while (true)
+	{
+		cout << "Masukkan jumlah data pada Array : ";		//Membuat inputan jumlah element Array
+		cin >> n;											// Memanggil variabel inputan n
+
+		if (n <= 20)
+		{	// Membuat kondisi n tidak lebih dari 20
+			break;
+
+		}
+		
